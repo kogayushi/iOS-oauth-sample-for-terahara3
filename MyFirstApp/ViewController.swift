740fb6775326d06f09b070f99878885b2015a59c
kogayushi <youshe.old.pleasure@gmail.com>
2018-03-18 13:47:42 +0900

    initial commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
-
+    
+    
+    var session: SFAuthenticationSession? = nil
+    
+    @IBAction func changeLabel(_ sender: Any) {
+        
+        let url = URL(string: "http://localhost:8080/login")!
+        self.session = SFAuthenticationSession(url: url, callbackURLScheme: "my-test-app") { url, error in
+            var fragm = url?.fragment
+            print(fragm!)
+        }
+        session?.start()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
